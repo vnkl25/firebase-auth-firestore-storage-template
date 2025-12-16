9b7caba39920580d1b4580fb620fc867eb8053d0
vnkl25 <vykle@lewisu.edu>
2025-12-16 09:23:41 -0800

    Initial commit

---
+++ public/firebase-config.js
@@ -0,0 +1,28 @@
+// Import the Firebase SDKs 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-storage.js";
+
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export so other scripts can use it
+export { auth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged };
+export { db, doc, setDoc, collection, getDocs };
+export { storage, ref, uploadBytes, getDownloadURL, listAll } 
+
